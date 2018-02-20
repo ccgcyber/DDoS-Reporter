c750ade885c1f5a666735204a533de1d4741ee94
CCG CyberDefense Group <ccg.cyber@gmail.com>
2018-02-20 10:10:52 -0500

    update text

--- ddosreporter/settings.py
+++ ddosreporter/settings.py
@@ -13,24 +13,29 @@ EMAIL_PASSWORD = ('sysadm@gmail.com', 'senha')
 SYSADM = ('sysadm@gmail.com', )
 
 #Demand limit for a single IP
-LIMITE_REQUISICOES_POR_IP = 150
+REQUIREMENT_LIMIT_FOR_IP = 150
+#LIMITE_REQUISICOES_POR_IP = 150
 
 #Limit of distinct requests that the server can support
-LIMITE_REQUISICOES_TOTAL = 180
+REQUIREMENT_LIMIT_TOTAL = 180
+#LIMITE_REQUISICOES_TOTAL = 180
 
 #Location of the apache log file (Default -> /var/log/apache2/access.log)
-ARQUIVO_DE_LOG = '/var/log/apache2/access.log'
+ARCHIVE_OF_LOG = '/var/log/apache2/access.log'
+#ARQUIVO_DE_LOG = '/var/log/apache2/access.log'
 
 #Block IPs so they no longer receive requests
 #True to Block, False to not Block
-BLOQUEAR_ATAQUES = False
+BLOCK_ATTACKS = False
+#BLOQUEAR_ATAQUES = False
 
 #Iptables rule to block IPs
 #(Default -> iptables -D INPUT -s <ip> -j DROP)
 IPTABLES = 'iptables -I INPUT -s <ip> -j DROP'
 
 #Time interval for log re-reading (time in seconds)
-INTERVALO_TEMPO = 1
+TIME_INTERVAL = 1
+#INTERVALO_TEMPO = 1
 
 # List blocked IPs:
 # iptables -L INPUT -n --line-numbers
