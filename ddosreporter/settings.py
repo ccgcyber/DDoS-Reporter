77f48adc5ef2f0851c51d5db2ccf7993d9a48911
CCG CyberDefense Group <ccg.cyber@gmail.com>
2018-02-20 09:42:07 -0500

    update text

--- ddosreporter/settings.py
+++ ddosreporter/settings.py
@@ -2,37 +2,37 @@
 
 #Author: Mateus Ferreira Silva <mtsferreirasilva@gmail.com>
 
-#Envia emails para os SYSADMs cadastrados a cada alerta de ataque detectado
-#True para enviar emails, False para não enviar
+#Send emails to SYSADMs registered to each detected attack alert
+#True to send emails, False not to send
 SEND_EMAIL = False
 
-#Email(gmail) e senha do email que enviará o alerta de ataques (servidor)
+#Email (gmail) and email password that will send the attack alert (server)
 EMAIL_PASSWORD = ('sysadm@gmail.com', 'senha')
 
-#Lista de emails de SYSADMs
+#SYSADMs mailing list
 SYSADM = ('sysadm@gmail.com', )
 
-#Limite de requisições para um único IP
+#Demand limit for a single IP
 LIMITE_REQUISICOES_POR_IP = 150
 
-#Limite de requisições distintas que o servidor pode suportar
+#Limit of distinct requests that the server can support
 LIMITE_REQUISICOES_TOTAL = 180
 
-#Localização do arquivo de log do apache (Padrão->/var/log/apache2/access.log)
+#Location of the apache log file (Default -> /var/log/apache2/access.log)
 ARQUIVO_DE_LOG = '/var/log/apache2/access.log'
 
-#Bloquear IPs para que não mais receber requisições
-#True para bloquear, False para não bloquear
+#Block IPs so they no longer receive requests
+#True to Block, False to not Block
 BLOQUEAR_ATAQUES = False
 
-#Regra de iptables para bloquear IPs
-#(Padrão -> iptables -D INPUT -s <ip> -j DROP)
+#Iptables rule to block IPs
+#(Default -> iptables -D INPUT -s <ip> -j DROP)
 IPTABLES = 'iptables -I INPUT -s <ip> -j DROP'
 
-#Intervalo de tempo para releitura do log (tempo em segundos)
+#Time interval for log re-reading (time in seconds)
 INTERVALO_TEMPO = 1
 
-# Listar IPs bloqueados:
+# List blocked IPs:
 # iptables -L INPUT -n --line-numbers
-# Liberar IPs usando número de identificação da lista:
-# iptables -D INPUT <numero>
+# Release IPs using list ID number:
+# iptables -D INPUT <number>
